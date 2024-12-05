b762895439593fed9cbae75c793d85cbde3aa496
Xprriacst <expertiaen5min@gmail.com>
2024-12-05 23:39:59 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import DesktopLayout from './components/DesktopLayout';
 import MobileLayout from './components/MobileLayout';
+import Conversations from './pages/Conversations';
 
 const App: React.FC = () => {
   const isMobile = window.innerWidth <= 768;
 
   return (
     <Router>
-      {isMobile ? <MobileLayout /> : <DesktopLayout />}
+      {isMobile ? (
+        <Routes>
+          <Route path="/properties/:propertyId/conversations" element={<Conversations />} />
+          <Route path="/" element={<MobileLayout />} />
+        </Routes>
+      ) : (
+        <Routes>
+          <Route path="/properties/:propertyId/conversations" element={<Conversations />} />
+          <Route path="/" element={<DesktopLayout />} />
+        </Routes>
+      )}
     </Router>
   );
 };
