f6068c1508cdb0efd64175b5c3bb242521201acb
Xprriacst <expertiaen5min@gmail.com>
2024-12-08 19:16:47 +0100

    Update DesktopLayout.tsx

--- src/components/DesktopLayout.tsx
+++ src/components/DesktopLayout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import Properties from '../pages/desktop/Properties';
 import Conversations from '../pages/Conversations';
@@ -7,6 +7,7 @@ import Settings from '../pages/Settings';
 import MobileChat from '../pages/MobileChat';
 import ChatSandbox from '../pages/ChatSandbox';
 import EmergencyCases from '../pages/EmergencyCases';
+import ConversationDetail from '../pages/ConversationDetail';
 
 const DesktopLayout: React.FC = () => {
   return (
@@ -15,17 +16,16 @@ const DesktopLayout: React.FC = () => {
         <Sidebar />
       </div>
       <main className="flex-1 overflow-auto ml-64">
-        <div className="p-6">
-          <Routes>
-            <Route path="/" element={<Properties />} />
-            <Route path="/conversations" element={<Conversations />} />
-            <Route path="/conversations/:propertyId" element={<Conversations />} />
-            <Route path="/chat/:conversationId" element={<MobileChat />} />
-            <Route path="/emergency" element={<EmergencyCases />} />
-            <Route path="/sandbox" element={<ChatSandbox />} />
-            <Route path="/settings" element={<Settings />} />
-          </Routes>
-        </div>
+        <Routes>
+          <Route path="/" element={<Properties />} />
+          <Route path="/conversations" element={<Conversations />} />
+          <Route path="/properties/:propertyId/conversations" element={<Conversations />} />
+          <Route path="/properties/:propertyId/conversations/:conversationId" element={<ConversationDetail />} />
+          <Route path="/emergency" element={<EmergencyCases />} />
+          <Route path="/sandbox" element={<ChatSandbox />} />
+          <Route path="/settings" element={<Settings />} />
+        </Routes>
+        <Outlet />
       </main>
     </div>
   );
