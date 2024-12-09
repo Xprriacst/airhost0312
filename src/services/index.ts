c7d0a0588f29bbc5cbb1990a4211f45a960cd210
Xprriacst <expertiaen5min@gmail.com>
2024-12-09 16:22:51 +0100

    Update index.ts

--- src/services/index.ts
+++ src/services/index.ts
@@ -2,10 +2,14 @@ import { propertyService } from './airtable/propertyService';
 import { conversationService } from './conversationService';
 import { messageService } from './messageService';
 import { aiService } from './ai/aiService';
+import { aiInstructionService } from './airtable/aiInstructionService';
+import { faqService } from './airtable/faqService';
 
 export {
   propertyService,
   conversationService,
   messageService,
-  aiService
+  aiService,
+  aiInstructionService,
+  faqService
 };
